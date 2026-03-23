769f7e474e64394672bebfbee5609b1240c07956
Nguyen-Duong-Trong-Tin <tinht5667@gmail.com>
2026-03-23 09:21:02 +0700

    update

--- index.js
+++ index.js
@@ -1,45 +1,98 @@
+import { getRandomInt } from "./helpers/random.js";
+
 Vue.createApp({
   data() {
     return {
-      count: 0
-    }
+      fullName: "Nguyen Duong Trong Tin",
+      myClass: 334,
+    };
+  },
+}).mount("#interpolation");
+
+Vue.createApp({
+  data() {
+    return {
+      imageUrl:
+        "https://res.cloudinary.com/df7e20fdm/image/upload/v1736868496/v3wkykrgehd8ut3izabt.jpg",
+      wikiUrl: "https://en.wikipedia.org/wiki/Jack_%E2%80%93_J97",
+    };
+  },
+}).mount("#bind-attribute");
+
+Vue.createApp({
+  methods: {
+    generatePoints() {
+      let points = 0;
+
+      for (let i = 0; i < 3; i++) {
+        const number = getRandomInt(1, 7);
+        points += number;
+      }
+
+      return points;
+    },
+    renderTaiHayXiu() {
+      const points = this.generatePoints();
+
+      if (points > 11) {
+        return "Tai " + points + " points";
+      }
+
+      return "Xiu " + points + " points";
+    },
+  },
+}).mount("#method");
+
+Vue.createApp({
+  data() {
+    return {
+      message: "<h1>Learning VueJS is the best</h1>",
+    };
+  },
+}).mount("#raw-html");
+
+Vue.createApp({
+  data() {
+    return {
+      count: 0,
+    };
   },
   methods: {
     handleClickMe() {
-      alert('You clicked me!');
+      alert("You clicked me!");
     },
     handleIncrease(quantity) {
       this.count += quantity;
     },
     handleSubmit() {
-      alert('Form submitted!');
+      alert("Form submitted!");
     },
     handleLogin() {
-      alert('Logged in!');
-    }
-  }
-}).mount('#event-handling');
+      alert("Logged in!");
+    },
+  },
+}).mount("#event-handling");
 
 Vue.createApp({
   data() {
     return {
-      message: ""
-    }
+      message: "",
+    };
   },
-}).mount('#two-way-binding');
+}).mount("#two-way-binding");
 
 Vue.createApp({
   data() {
     return {
       count: 0,
       firstName: "John",
-      lastName: "Doe"
-    }
+      lastName: "Doe",
+    };
   },
   watch: {
     count(newValue, oldValue) {
       console.log(newValue, oldValue);
-    }
+    },
   },
   computed: {
     fullName() {
@@ -49,9 +102,9 @@ Vue.createApp({
   methods: {
     increseCount() {
       ++this.count;
-    }
-  }
-}).mount('#computed-and-watch');
+    },
+  },
+}).mount("#computed-and-watch");
 
 Vue.createApp({
   data() {
@@ -59,14 +112,14 @@ Vue.createApp({
       styleForP: {
         backgroundColor: "red",
         color: "blue",
-        fontSize: "20px"
+        fontSize: "20px",
       },
       styleForText: {
         textAlign: "left",
-        fontWeight: "bold"
+        fontWeight: "bold",
       },
-      active: false
-    }
+      active: false,
+    };
   },
   methods: {
     handleFormat() {
@@ -75,6 +128,6 @@ Vue.createApp({
     },
     handleActive() {
       this.active = true;
-    }
-  }
-}).mount('#styling');
+    },
+  },
+}).mount("#styling");
